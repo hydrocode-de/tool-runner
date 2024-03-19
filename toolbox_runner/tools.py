779a9745a44083368603b96bba779c134e5e21d9
Mirko Mälicke <mirko@hydrocode.de>
2024-03-19 14:40:51 +0100

    new tool Sniffer

---
+++ toolbox_runner/tools.py
@@ -0,0 +1,58 @@
+from typing import List
+
+from pydantic import BaseModel
+from yaml import load, Loader
+
+from toolbox_runner.docker import get_client
+from toolbox_runner.models import Tool
+
+
+class ToolSniffer(BaseModel):
+    docker_image: str
+
+
+    def _get_tool_config(self) -> dict:
+        # get a docker client
+        client = get_client()
+
+        # check out the yaml in the image
+        try:
+            raw = client.containers.run(self.docker_image, command='cat /src/tool.yml', remove=True)
+        # TODO: change this to the Exception, that occures when the tool.yml is not there
+        except Exception:
+            return {}
+
+        # parse the yaml
+        conf = load(raw, Loader=Loader)
+
+        return conf
+
+
+    def get_tools(self) -> List[str]:
+        conf = self._get_tool_config()
+
+        # the tools are listed as keys in the 'tools' dictionary
+        return list(conf['tools'].keys())
+
+
+    def tool(self, name: str) -> Tool:
+        # check out the yaml in the image
+        conf = self._get_tool_config()
+
+        # check that the tool is in the keys
+        if name not in conf['tools']:
+            raise RuntimeError(f'Tool {name} not found in {self.docker_image}')
+        
+        # build the tool payload
+        payload = {**conf['tools'][name], 'docker_image': self.docker_image, 'name': name}
+        
+        # update the payload for the parameter names
+        for k, v in payload['parameters'].items():
+            payload['parameters'][k].update({'name': k})
+
+        # use the Tool model to validate everything
+        tool = Tool.model_validate(payload)
+
+        return tool
+
+
